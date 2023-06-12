5cbf611890e5af6998b21647c06410176a95af83
mohith2017 <mohith.m.venkat@gmail.com>
2023-06-12 19:07:54 -0400

    payload cors

--- functions/server.js
+++ functions/server.js
@@ -19,10 +19,11 @@ const { ApiError, client: square } = require('./server/square');
 
 async function createPayment(req, res) {
 
-  res.setHeader('Access-Control-Allow-Origin', 'https://glittery-sunshine-fd9cac.netlify.app');
+  // res.setHeader('Access-Control-Allow-Origin', 'https://glittery-sunshine-fd9cac.netlify.app');
   
   const payload = await json(req);
   logger.debug(JSON.stringify(payload));
+  console.log(JSON.stringify(payload))
   // We validate the payload for specific fields. You may disable this feature
   // if you would prefer to handle payload validation on your own.
   if (!validatePaymentPayload(payload)) {
