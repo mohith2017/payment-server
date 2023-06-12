bdb722ec236e9f3c7ffc94e3355e516583cfd8d9
mohith2017 <mohith.m.venkat@gmail.com>
2023-06-12 14:23:36 -0400

    Allowed CORS

--- functions/server.js
+++ functions/server.js
@@ -18,6 +18,8 @@ const {
 const { ApiError, client: square } = require('./server/square');
 
 async function createPayment(req, res) {
+
+  res.setHeader('Access-Control-Allow-Origin', 'https://master--glittery-sunshine-fd9cac.netlify.app');
   const payload = await json(req);
   logger.debug(JSON.stringify(payload));
   // We validate the payload for specific fields. You may disable this feature
